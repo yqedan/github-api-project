6d64145a5283527ac2f382911d4258d54e426e47
Yusuf Qedan <yusuf9191@gmail.com>
2016-10-14 14:14:17 -0700

    changed formatting in output from a list to a table

--- js/github-user-lookup-interface.js
+++ js/github-user-lookup-interface.js
@@ -1,13 +1,27 @@
 var GithubLookup = require('./../js/github-user-lookup.js').githubLookupModule;
 
-function displayRepos(repos){
+function displayRepos(repos, page){
   $("#data").text("");
   var repoList = "";
   for (var i = 0; i < repos.length; i++) {
-    repoList += "<li>Name: " + repos[i].name + "</li>";
-    repoList += "<li>Description: " + repos[i].description + "</li>";
+    repoList += "<tr>";
+    repoList += "<td>" + ((page-1)*30 + (i+1)) + "</td>";
+    repoList += "<td>" + repos[i].name + "</td>";
+    if (repos[i].description === null) {
+      repoList += "<td>No Description </td>";
+    }else{
+      repoList += "<td>" + repos[i].description + "</td>";
+    }
+    repoList += "</tr>";
   }
-  $("#data").append("<ul>"+ repoList + "</ul>");
+  $("#data").append("<table class='table'>"+
+                    "<tr>" +
+                    "<th>#</th>" +
+                    "<th>Name</th>" +
+                    "<th>Description</th>" +
+                    "</tr>" +
+                    repoList +
+                    "</table>");
 }
 
 function cacheRepos(repos, page){
@@ -16,17 +30,16 @@ function cacheRepos(repos, page){
 
 function displayPageList(username, pageCount){
   $("#page-buttons").text("");
+  $("#data").hide();
   $("#page-buttons").append("<h3>Please select a page!</h3>");
   for (var i = 1; i <= pageCount; i++) {
-    $("#page-buttons").append(
-      "<button class='btn view-page' id='view-page-" + i + "'>Page " + i + "</button>&nbsp;"
-    );
+    $("#page-buttons").append("<button class='btn view-page' id='view-page-" + i + "'>Page " + i + "</button>&nbsp;");
     githublookup.getPageOfRepos(username, cacheRepos, i);
   }
   $(".view-page").click(function() {
     $("#data").show();
     var page = $(this).attr('id')[10];
-    displayRepos(pages[page-1]);
+    displayRepos(pages[page-1], page);
   });
   $("#page-buttons").show();
 }
