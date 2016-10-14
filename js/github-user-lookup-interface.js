8e091ca1c6375cb793cf2822275dcf54c5638cb2
Yusuf Qedan <yusuf9191@gmail.com>
2016-10-14 10:01:46 -0700

    add behavior to request number of public repos so I can display more than 30

--- js/github-user-lookup-interface.js
+++ js/github-user-lookup-interface.js
@@ -8,11 +8,16 @@ function displayRepos(repos){
   }
   $("#data").append("<ul>"+ repoList + "</ul>");
 }
+function displayPageList(pageCount){
+
+}
 $(document).ready(function() {
   $("#get-user-info").submit(function(event){
     event.preventDefault();
+    $("#data").text("");
     var username = $("#username").val();
     var githublookup = new GithubLookup();
-    githublookup.getRepos(username, displayRepos);
+    //githublookup.getRepos(username, displayRepos);
+    githublookup.getNumberOfPagesOfRepos(username);
   });
 });
--- js/github-user-lookup.js
+++ js/github-user-lookup.js
@@ -1,11 +1,21 @@
 var apiKey = require('./../.env').apiKey;
 
 GithubLookup = function(){
+  this.numberOfpagesOfRepos = 0;
+}
 
+GithubLookup.prototype.getNumberOfPagesOfRepos = function(username){
+  var that = this;
+  $.get('https://api.github.com/users/' + username + '?&access_token=' + apiKey).then(function(response){
+    console.log(response);//.public_repos)
+  }).fail(function(error){
+    displayRepos(error.responseJSON.message);
+  });
 }
 
 GithubLookup.prototype.getRepos = function(username, displayRepos){
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+
+  $.get('https://api.github.com/users/' + username + '/repos?per_page=100&access_token=' + apiKey).then(function(response){
     displayRepos(response);
   }).fail(function(error){
     displayRepos(error.responseJSON.message);
